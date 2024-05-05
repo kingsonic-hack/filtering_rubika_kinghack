f5ed9d80dce16304b68906f3e6b14d7a747e2c0f
kingsonic-hack <168739464+kingsonic-hack@users.noreply.github.com>
2024-05-06 00:46:05 +0330

    Add files via upload

---
+++ king_filter.py
@@ -0,0 +1,80 @@
+from time import sleep
+from os import system
+import random
+import sys
+import os
+from colorama import Fore
+
+sleep(1)
+
+print("                           start           ")
+sleep(1.5)
+print("")
+
+sleep(.3)
+
+print("")
+sleep(.3)
+
+
+print(f"""                    ꧁kingsonic_hack꧂        """)
+sleep(2)
+print("")
+sleep(.3)
+print("")
+print(Fore.LIGHTGREEN_EX+
+"             rubika id: @king_sonic127")
+sleep(.2)
+print("                    Channel tutorial: @filter_tutorial_king ")
+sleep(1)
+code = input(Fore.LIGHTBLUE_EX+f"""
+(1) filter account
+(2) filter channel 
+(3) super filter
+select>>>""")
+
+if code == "1":
+	sleep(.3)
+	
+	print(Fore.BLUE+"loadonig...")
+	sleep(2)
+	print("")
+	print(Fore.LIGHTBLUE_EX+"your filter code: ")
+	print(Fore.RED+"(1.1.1.2.3.4.5.6.5.4.3.4.6.7.4/////f///i///l//filter////.1/1.9.4.4.4.1.1.1.1.1.1.1.1.1.9.9.8.7.5.3.3.2)")
+	
+	sleep(2)
+	print("")
+	
+	print("     END HACK    ")
+	
+	sleep(4)
+	
+if code == "3":
+		sleep(.3)
+		
+		print(Fore.BLUE+"loading...")
+		sleep(2)
+		print("")
+		print(Fore.LIGHTBLUE_EX+"your filter code: ")
+		print(Fore.RED+"(1.1.1.1.1.1.1.1.1.1.1.4.3.1/////fu///ck///.4.6.8.9.0.1.1.2.3.5//////filtr///ing/////f/1.2.3/y/1.3.////y/////1.1.1.2.3.4.5.6.7.8//////f/////.9.9.1.1.1.0.0.0.2.3.4.5)")
+		
+		print("")
+		sleep(2)
+		
+		print("     END HACK")
+		
+		sleep(4)
+
+if code == "2":
+        sleep(.3)
+        print(Fore.BLUE+"loading...")
+        sleep(2)
+        print("")
+        print(Fore.LIGHTBLUE_EX+"youre filter code:")
+        print(Fore.RED+" (1.1.1.2.2.2.2.2.3.3.3.3.3.4.5.6.7./////f////i////l//0.7.5.3.2.1.4.6.4.3/.1////f/.1//.1/.1/f/u//////y//////f/f/f/f//////1.2.3.6.3.1/////fil////ter///.1.2.3.2.1.3.4.5.6/f/.1.5) ")
+        print("")
+        sleep(2)
+        
+        print("     END HACK   ")
+        
+        sleep(4)
